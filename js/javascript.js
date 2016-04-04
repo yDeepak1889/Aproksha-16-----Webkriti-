ff8e1f2e9fba03e996aa8874633bc7ba3d921ce3
Deepak kumar yadav <ydeepak1889@gmail.com>
2016-04-05 00:01:17 +0530

    Create javascript.js

---
+++ js/javascript.js
@@ -0,0 +1,583 @@
+$(document).ready(function(){
+	$("#new_trend").hover(function(){
+		$(this).html("<h1>SHOP THE TREND</h1><a href='trendy.html'><button class='btn btn-lg btn_add'>SHOP NOW</button></a>").addClass('hover_div')},
+			function(){
+				$(this).html('').removeClass('hover_div');
+			});
+
+
+	$("#festival").hover(function(){
+		$(this).html("<h1>FESTIVAL SPECIAL 50 % OFF</h1><a href='festival.html'><button class='btn btn-lg btn_add'>SHOP NOW</button></a>").addClass('hover_div')},
+			function(){
+				$(this).html('').removeClass('hover_div');
+			});
+
+
+
+	$("#formal").hover(function(){
+		$(this).html("<h1>WIDE RANGE OF MAN'S FORMAL</h1><a href='formal.html'><button class='btn btn-lg btn_add'>SHOP NOW</button></a>").addClass('hover_div')},
+			function(){
+				$(this).html('').removeClass('hover_div');
+			});
+
+	
+});
+
+
+
+$(document).ready(function(){
+
+	$("#new_1").hover(function(){
+		$(this).html("<h1>Led Zeppelin Tee</h1><button class='btn btn-lg btn_add'>Rs.4,578</button>").addClass('hover_div')},
+			function(){
+				$(this).html('').removeClass('hover_div');
+			});
+
+
+	$("#new_2").hover(function(){
+		$(this).html("<h1>Nirvana Short Tee</h1><button class='btn btn-lg btn_add'>Rs.4,675</button>").addClass('hover_div')},
+			function(){
+				$(this).html('').removeClass('hover_div');
+			});
+
+	$("#new_6").hover(function(){
+		$(this).html("<h1>Starry Night Capelet</h1><button class='btn btn-lg btn_add'>Rs.19,936</button>").addClass('hover_div')},
+			function(){
+				$(this).html('').removeClass('hover_div');
+			});
+
+	$("#new_4").hover(function(){
+		$(this).html("<h1>Hey Joe Cropped Flare</h1><button class='btn btn-lg btn_add'>Rs.11,345</button>").addClass('hover_div')},
+			function(){
+				$(this).html('').removeClass('hover_div');
+			});
+
+	$("#new_5").hover(function(){
+		$(this).html("<h1>Madden High Rise</h1><button class='btn btn-lg btn_add'>Rs.4,432	</button>").addClass('hover_div')},
+			function(){
+				$(this).html('').removeClass('hover_div');
+			});
+
+	$("#new_3").hover(function(){
+		$(this).html("<h1>Salvage Bandit Cut Offs</h1><button class='btn btn-lg btn_add'>Rs.7,656</button>").addClass('hover_div')},
+			function(){
+				$(this).html('').removeClass('hover_div');
+			});
+
+	$("#new_7").hover(function(){
+		$(this).html("<h1>Drop Waist Soft Jacket</h1><button class='btn btn-lg btn_add'>Rs.33,471</button>").addClass('hover_div')},
+			function(){
+				$(this).html('').removeClass('hover_div');
+			});
+
+	$("#new_8").hover(function(){
+		$(this).html("<h1>Shrunken Military Blazer</h1><button class='btn btn-lg btn_add'>Rs.9,901</button>").addClass('hover_div')},
+			function(){
+				$(this).html('').removeClass('hover_div');
+			});
+
+	$("#new_9").hover(function(){
+		$(this).html("<h1>Have Your Fun Romper</h1><button class='btn btn-lg btn_add'>Rs.7,225</button>").addClass('hover_div')},
+			function(){
+				$(this).html('').removeClass('hover_div');
+			});
+
+
+});
+
+
+
+$(document).ready(function(){
+
+		$(document).ready(function(){
+
+	$("#new_m_1").hover(function(){
+		$(this).html("<h1>Mission Crew Tee</h1><button class='btn btn-lg btn_add'>Rs.1,578</button>").addClass('hover_div')},
+			function(){
+				$(this).html('').removeClass('hover_div');
+			});
+
+
+	$("#new_m_2").hover(function(){
+		$(this).html("<h1>Hawaiian long Shirt</h1><button class='btn btn-lg btn_add'>Rs.3,675</button>").addClass('hover_div')},
+			function(){
+				$(this).html('').removeClass('hover_div');
+			});
+
+	$("#new_m_3").hover(function(){
+		$(this).html("<h1>Packable Anorak</h1><button class='btn btn-lg btn_add'>Rs.1,936</button>").addClass('hover_div')},
+			function(){
+				$(this).html('').removeClass('hover_div');
+			});
+
+	$("#new_m_4").hover(function(){
+		$(this).html("<h1>Selvedge Sunset 1 Pocket Shirt</h1><button class='btn btn-lg btn_add'>Rs.6,345</button>").addClass('hover_div')},
+			function(){
+				$(this).html('').removeClass('hover_div');
+			});
+
+	$("#new_m_5").hover(function(){
+		$(this).html("<h1>Utility Short</h1><button class='btn btn-lg btn_add'>Rs.2,432	</button>").addClass('hover_div')},
+			function(){
+				$(this).html('').removeClass('hover_div');
+			});
+
+	$("#new_m_6").hover(function(){
+		$(this).html("<h1>Denim Trucker Jacket</h1><button class='btn btn-lg btn_add'>Rs.5,656</button>").addClass('hover_div')},
+			function(){
+				$(this).html('').removeClass('hover_div');
+			});
+
+	$("#new_m_7").hover(function(){
+		$(this).html("<h1>Crewneck Sweatshirt</h1><button class='btn btn-lg btn_add'>Rs.3,471</button>").addClass('hover_div')},
+			function(){
+				$(this).html('').removeClass('hover_div');
+			});
+
+	$("#new_m_8").hover(function(){
+		$(this).html("<h1>The Trucker Jacket</h1><button class='btn btn-lg btn_add'>Rs.4,901</button>").addClass('hover_div')},
+			function(){
+				$(this).html('').removeClass('hover_div');
+			});
+
+	$("#new_m_9").hover(function(){
+		$(this).html("<h1>Barstow Western Shirt</h1><button class='btn btn-lg btn_add'>Rs.2,225</button>").addClass('hover_div')},
+			function(){
+				$(this).html('').removeClass('hover_div');
+			});
+
+
+});
+
+
+});
+
+
+$(document).ready(function(){
+
+	$("#jean_m_1").hover(function(){
+		$(this).html("<h1>Fit Selvedge Jeans</h1><button class='btn btn-lg btn_add'>Rs.4,578</button>").addClass('hover_div')},
+			function(){
+				$(this).html('').removeClass('hover_div');
+			});
+
+
+	$("#jean_m_2").hover(function(){
+		$(this).html("<h1>Slim Fit Jeans</h1><button class='btn btn-lg btn_add'>Rs.3,675</button>").addClass('hover_div')},
+			function(){
+				$(this).html('').removeClass('hover_div');
+			});
+
+	$("#jean_m_3").hover(function(){
+		$(this).html("<h1>Slim Fit Trouser</h1><button class='btn btn-lg btn_add'>Rs.1,936</button>").addClass('hover_div')},
+			function(){
+				$(this).html('').removeClass('hover_div');
+			});
+
+	$("#jean_m_4").hover(function(){
+		$(this).html("<h1>Athletic Fit Jean</h1><button class='btn btn-lg btn_add'>Rs.3,345</button>").addClass('hover_div')},
+			function(){
+				$(this).html('').removeClass('hover_div');
+			});
+
+	$("#jean_m_5").hover(function(){
+		$(this).html("<h1>Slim Fit Trousers</h1><button class='btn btn-lg btn_add'>Rs.2,432	</button>").addClass('hover_div')},
+			function(){
+				$(this).html('').removeClass('hover_div');
+			});
+
+	$("#jean_m_6").hover(function(){
+		$(this).html("<h1>Slim Fit Trousers</h1><button class='btn btn-lg btn_add'>Rs.2,656</button>").addClass('hover_div')},
+			function(){
+				$(this).html('').removeClass('hover_div');
+			});
+
+	$("#jean_m_7").hover(function(){
+		$(this).html("<h1>Slim Straight Jeans</h1><button class='btn btn-lg btn_add'>Rs.3,471</button>").addClass('hover_div')},
+			function(){
+				$(this).html('').removeClass('hover_div');
+			});
+
+	$("#jean_m_8").hover(function(){
+		$(this).html("<h1>Fit Light Jeans</h1><button class='btn btn-lg btn_add'>Rs.4,901</button>").addClass('hover_div')},
+			function(){
+				$(this).html('').removeClass('hover_div');
+			});
+
+	$("#jean_m_9").hover(function(){
+		$(this).html("<h1>Slim MOtion Jeans</h1><button class='btn btn-lg btn_add'>Rs.2,225</button>").addClass('hover_div')},
+			function(){
+				$(this).html('').removeClass('hover_div');
+			});
+
+
+});
+
+
+
+
+$(document).ready(function(){
+
+	$("#jean_w_1").hover(function(){
+		$(this).html("<h1>Supper Skinny Jeans</h1><button class='btn btn-lg btn_add'>Rs.4,578</button>").addClass('hover_div')},
+			function(){
+				$(this).html('').removeClass('hover_div');
+			});
+
+
+	$("#jean_w_2").hover(function(){
+		$(this).html("<h1>Flawless Fit Jeans</h1><button class='btn btn-lg btn_add'>Rs.3,675</button>").addClass('hover_div')},
+			function(){
+				$(this).html('').removeClass('hover_div');
+			});
+
+	$("#jean_w_3").hover(function(){
+		$(this).html("<h1>Cropped Skinny Jeans</h1><button class='btn btn-lg btn_add'>Rs.1,936</button>").addClass('hover_div')},
+			function(){
+				$(this).html('').removeClass('hover_div');
+			});
+
+	$("#jean_w_4").hover(function(){
+		$(this).html("<h1>Slim Fit jean</h1><button class='btn btn-lg btn_add'>Rs.3,345</button>").addClass('hover_div')},
+			function(){
+				$(this).html('').removeClass('hover_div');
+			});
+
+	$("#jean_w_5").hover(function(){
+		$(this).html("<h1>Shaping Slim Jeans</h1><button class='btn btn-lg btn_add'>Rs.2,432	</button>").addClass('hover_div')},
+			function(){
+				$(this).html('').removeClass('hover_div');
+			});
+
+	$("#jean_w_6").hover(function(){
+		$(this).html("<h1>Wedgie Fit Jeans</h1><button class='btn btn-lg btn_add'>Rs.2,656</button>").addClass('hover_div')},
+			function(){
+				$(this).html('').removeClass('hover_div');
+			});
+
+	$("#jean_w_7").hover(function(){
+		$(this).html("<h1>Relaxed Straight Jeans</h1><button class='btn btn-lg btn_add'>Rs.3,471</button>").addClass('hover_div')},
+			function(){
+				$(this).html('').removeClass('hover_div');
+			});
+
+	$("#jean_w_8").hover(function(){
+		$(this).html("<h1>Melrose Line Jeans</h1><button class='btn btn-lg btn_add'>Rs.4,901</button>").addClass('hover_div')},
+			function(){
+				$(this).html('').removeClass('hover_div');
+			});
+
+	$("#jean_w_9").hover(function(){
+		$(this).html("<h1>Wool Chino Trousers</h1><button class='btn btn-lg btn_add'>Rs.2,225</button>").addClass('hover_div')},
+			function(){
+				$(this).html('').removeClass('hover_div');
+			});
+
+
+});
+
+
+
+$(document).ready(function(){
+
+	$("#kid_1").hover(function(){
+		$(this).html("<h1>Sweatheart Knit Jogger</h1><button class='btn btn-lg btn_add'>Rs.1,578</button>").addClass('hover_div')},
+			function(){
+				$(this).html('').removeClass('hover_div');
+			});
+
+
+	$("#kid_2").hover(function(){
+		$(this).html("<h1>Destructed denim Leggings </h1><button class='btn btn-lg btn_add'>Rs.1,675</button>").addClass('hover_div')},
+			function(){
+				$(this).html('').removeClass('hover_div');
+			});
+
+	$("#kid_3").hover(function(){
+		$(this).html("<h1>Knit Jogger</h1><button class='btn btn-lg btn_add'>Rs.1,936</button>").addClass('hover_div')},
+			function(){
+				$(this).html('').removeClass('hover_div');
+			});
+
+	$("#kid_4").hover(function(){
+		$(this).html("<h1>Summer Love Shorty Shorts</h1><button class='btn btn-lg btn_add'>Rs.1,345</button>").addClass('hover_div')},
+			function(){
+				$(this).html('').removeClass('hover_div');
+			});
+
+	$("#kid_5").hover(function(){
+		$(this).html("<h1>Skater Skirt</h1><button class='btn btn-lg btn_add'>Rs.1,432	</button>").addClass('hover_div')},
+			function(){
+				$(this).html('').removeClass('hover_div');
+			});
+
+	$("#kid_6").hover(function(){
+		$(this).html("<h1>Rita Romper</h1><button class='btn btn-lg btn_add'>Rs.1,656</button>").addClass('hover_div')},
+			function(){
+				$(this).html('').removeClass('hover_div');
+			});
+
+	$("#kid_7").hover(function(){
+		$(this).html("<h1>Jumper</h1><button class='btn btn-lg btn_add'>Rs.999</button>").addClass('hover_div')},
+			function(){
+				$(this).html('').removeClass('hover_div');
+			});
+
+	$("#kid_8").hover(function(){
+		$(this).html("<h1>Trucker Vest</h1><button class='btn btn-lg btn_add'>Rs.2,901</button>").addClass('hover_div')},
+			function(){
+				$(this).html('').removeClass('hover_div');
+			});
+
+	$("#kid_9").hover(function(){
+		$(this).html("<h1>Knit Jacket</h1><button class='btn btn-lg btn_add'>Rs.2,225</button>").addClass('hover_div')},
+			function(){
+				$(this).html('').removeClass('hover_div');
+			});
+
+
+});
+
+
+
+$(document).ready(function(){
+
+	$("#trend_1").hover(function(){
+		$(this).html("<h1>Faye Boxy Shirt</h1><button class='btn btn-lg btn_add'>Rs.3,578</button>").addClass('hover_div')},
+			function(){
+				$(this).html('').removeClass('hover_div');
+			});
+
+
+	$("#trend_2").hover(function(){
+		$(this).html("<h1>Beverly Shirt</h1><button class='btn btn-lg btn_add'>Rs.4,675</button>").addClass('hover_div')},
+			function(){
+				$(this).html('').removeClass('hover_div');
+			});
+
+	$("#trend_3").hover(function(){
+		$(this).html("<h1>Tailored Western Shirt</h1><button class='btn btn-lg btn_add'>Rs.3,936</button>").addClass('hover_div')},
+			function(){
+				$(this).html('').removeClass('hover_div');
+			});
+
+	$("#trend_4").hover(function(){
+		$(this).html("<h1>French Terry Trucker</h1><button class='btn btn-lg btn_add'>Rs.5,345</button>").addClass('hover_div')},
+			function(){
+				$(this).html('').removeClass('hover_div');
+			});
+
+	$("#trend_5").hover(function(){
+		$(this).html("<h1>Bomber Jacket</h1><button class='btn btn-lg btn_add'>Rs.7,432	</button>").addClass('hover_div')},
+			function(){
+				$(this).html('').removeClass('hover_div');
+			});
+
+	$("#trend_6").hover(function(){
+		$(this).html("<h1>Barstow Western Shirt</h1><button class='btn btn-lg btn_add'>Rs.4,656</button>").addClass('hover_div')},
+			function(){
+				$(this).html('').removeClass('hover_div');
+			});
+
+	$("#trend_7").hover(function(){
+		$(this).html("<h1>Slim Cut Off Short</h1><button class='btn btn-lg btn_add'>Rs.9999</button>").addClass('hover_div')},
+			function(){
+				$(this).html('').removeClass('hover_div');
+			});
+
+	$("#trend_8").hover(function(){
+		$(this).html("<h1>Silk Jumpsuit</h1><button class='btn btn-lg btn_add'>Rs.6,901</button>").addClass('hover_div')},
+			function(){
+				$(this).html('').removeClass('hover_div');
+			});
+
+	$("#trend_9").hover(function(){
+		$(this).html("<h1>Iconic Western Dress</h1><button class='btn btn-lg btn_add'>Rs.7,225</button>").addClass('hover_div')},
+			function(){
+				$(this).html('').removeClass('hover_div');
+			});
+
+
+});
+
+$(document).ready(function(){
+
+	$("#fest_1").hover(function(){
+		$(this).html("<h1>Vency Design Sari </h1><button class='btn btn-lg btn_add'>Rs.8,578</button>").addClass('hover_div')},
+			function(){
+				$(this).html('').removeClass('hover_div');
+			});
+
+
+	$("#fest_2").hover(function(){
+		$(this).html("<h1>Laazree Sari</h1><button class='btn btn-lg btn_add'>Rs.6,675</button>").addClass('hover_div')},
+			function(){
+				$(this).html('').removeClass('hover_div');
+			});
+
+	$("#fest_3").hover(function(){
+		$(this).html("<h1>Design Villa Sari</h1><button class='btn btn-lg btn_add'>Rs.10,936</button>").addClass('hover_div')},
+			function(){
+				$(this).html('').removeClass('hover_div');
+			});
+
+	$("#fest_6").hover(function(){
+		$(this).html("<h1>Body Fit Salwaar Suit</h1><button class='btn btn-lg btn_add'>Rs.5,345</button>").addClass('hover_div')},
+			function(){
+				$(this).html('').removeClass('hover_div');
+			});
+
+	$("#fest_5").hover(function(){
+		$(this).html("<h1>Trishulom Salwaar Suit</h1><button class='btn btn-lg btn_add'>Rs.7,432</button>").addClass('hover_div')},
+			function(){
+				$(this).html('').removeClass('hover_div');
+			});
+
+	$("#fest_4").hover(function(){
+		$(this).html("<h1>Stylo Salwaar Suit</h1><button class='btn btn-lg btn_add'>Rs.4,656</button>").addClass('hover_div')},
+			function(){
+				$(this).html('').removeClass('hover_div');
+			});
+
+	$("#fest_7").hover(function(){
+		$(this).html("<h1>Khaan Saab Sherwani</h1><button class='btn btn-lg btn_add'>Rs.9999</button>").addClass('hover_div')},
+			function(){
+				$(this).html('').removeClass('hover_div');
+			});
+
+	$("#fest_8").hover(function(){
+		$(this).html("<h1>Formal Kurta</h1><button class='btn btn-lg btn_add'>Rs.2,901</button>").addClass('hover_div')},
+			function(){
+				$(this).html('').removeClass('hover_div');
+			});
+
+	$("#fest_9").hover(function(){
+		$(this).html("<h1>Baby Suit</h1><button class='btn btn-lg btn_add'>Rs.2,225</button>").addClass('hover_div')},
+			function(){
+				$(this).html('').removeClass('hover_div');
+			});
+
+
+});
+
+
+$(document).ready(function(){
+
+	$("#for_1").hover(function(){
+		$(this).html("<h1>British Dinner Jacket</h1><button class='btn btn-lg btn_add'>Rs.18,578</button>").addClass('hover_div')},
+			function(){
+				$(this).html('').removeClass('hover_div');
+			});
+
+
+	$("#for_2").hover(function(){
+		$(this).html("<h1>Hoursecut Dress</h1><button class='btn btn-lg btn_add'>Rs.16,675</button>").addClass('hover_div')},
+			function(){
+				$(this).html('').removeClass('hover_div');
+			});
+
+	$("#for_3").hover(function(){
+		$(this).html("<h1>Vellet Dinner Jacket</h1><button class='btn btn-lg btn_add'>Rs.10,936</button>").addClass('hover_div')},
+			function(){
+				$(this).html('').removeClass('hover_div');
+			});
+
+	$("#for_4").hover(function(){
+		$(this).html("<h1>Peak Lapel Jacket</h1><button class='btn btn-lg btn_add'>Rs.50,345</button>").addClass('hover_div')},
+			function(){
+				$(this).html('').removeClass('hover_div');
+			});
+
+	$("#for_5").hover(function(){
+		$(this).html("<h1>Peak Lapel Tuxedo</h1><button class='btn btn-lg btn_add'>Rs.17,432</button>").addClass('hover_div')},
+			function(){
+				$(this).html('').removeClass('hover_div');
+			});
+
+	$("#for_6").hover(function(){
+		$(this).html("<h1>Shawl Collar Waistcoat</h1><button class='btn btn-lg btn_add'>Rs.14,656</button>").addClass('hover_div')},
+			function(){
+				$(this).html('').removeClass('hover_div');
+			});
+
+	$("#for_7").hover(function(){
+		$(this).html("<h1>Breasted Morning Waistcoat</h1><button class='btn btn-lg btn_add'>Rs.19,999</button>").addClass('hover_div')},
+			function(){
+				$(this).html('').removeClass('hover_div');
+			});
+
+	$("#for_8").hover(function(){
+		$(this).html("<h1>Charcoal Morning Coat</h1><button class='btn btn-lg btn_add'>Rs.21,901</button>").addClass('hover_div')},
+			function(){
+				$(this).html('').removeClass('hover_div');
+			});
+
+	$("#for_9").hover(function(){
+		$(this).html("<h1>Yankey Tee Coat</h1><button class='btn btn-lg btn_add'>Rs.21,225</button>").addClass('hover_div')},
+			function(){
+				$(this).html('').removeClass('hover_div');
+			});
+
+
+});
+
+
+$(document).ready(function(){
+
+	$("#sum_1").hover(function(){
+		$(this).html("<h1>Midnight Blooms Dress</h1><button class='btn btn-lg btn_add'>Rs.18,578</button>").addClass('hover_div')},
+			function(){
+				$(this).html('').removeClass('hover_div');
+			});
+
+
+	$("#sum_2").hover(function(){
+		$(this).html("<h1>Ruffle Me Up Mini Dress</h1><button class='btn btn-lg btn_add'>Rs.16,675</button>").addClass('hover_div')},
+			function(){
+				$(this).html('').removeClass('hover_div');
+			});
+
+	$("#sum_3").hover(function(){
+		$(this).html("<h1>Sylvia Mini Dress</h1><button class='btn btn-lg btn_add'>Rs.10,936</button>").addClass('hover_div')},
+			function(){
+				$(this).html('').removeClass('hover_div');
+			});
+
+	$("#sum_4").hover(function(){
+		$(this).html("<h1>Hollywood Printed Top</h1><button class='btn btn-lg btn_add'>Rs.5,345</button>").addClass('hover_div')},
+			function(){
+				$(this).html('').removeClass('hover_div');
+			});
+
+	$("#sum_5").hover(function(){
+		$(this).html("<h1>Bermuda Shorts</h1><button class='btn btn-lg btn_add'>Rs.7,432</button>").addClass('hover_div')},
+			function(){
+				$(this).html('').removeClass('hover_div');
+			});
+
+	$("#sum_6").hover(function(){
+		$(this).html("<h1>Utility Shorts</h1><button class='btn btn-lg btn_add'>Rs.14,656</button>").addClass('hover_div')},
+			function(){
+				$(this).html('').removeClass('hover_div');
+			});
+
+	$("#sum_7").hover(function(){
+		$(this).html("<h1>Casuals Stripe T-Shirt </h1><button class='btn btn-lg btn_add'>Rs.1,999</button>").addClass('hover_div')},
+			function(){
+				$(this).html('').removeClass('hover_div');
+			});
+
+	$("#sum_8").hover(function(){
+		$(this).html("<h1>Graphic T-Shirt</h1><button class='btn btn-lg btn_add'>Rs.2,901</button>").addClass('hover_div')},
+			function(){
+				$(this).html('').removeClass('hover_div');
+			});
+
+	$("#sum_9").hover(function(){
+		$(this).html("<h1>Classic Pocket T-Shirt</h1><button class='btn btn-lg btn_add'>Rs.999</button>").addClass('hover_div')},
+			function(){
+				$(this).html('').removeClass('hover_div');
+			});
+
+
+});
